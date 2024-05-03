3aac2c24fc57e6ed31ee49cc37531e0d1f9bc9e4
Christopher Jongue <christopherjongue@gmail.com>
2024-05-03 15:53:48 +0800

    Added Game's elo & updated form on ad

--- app/routes.py
+++ app/routes.py
@@ -4,7 +4,11 @@ from app import app
 @app.route('/account')
 def account():
     return render_template("accountpage.html")
-
+csranks=['SILVER','GOLD NOVA','MASTER GUARDIAN','LEGENDARY']
+owranks=['BRONZE','SILVER','GOLD','PLATNIUM','DIAMOND','MASTER','GRANDMASTER','CHAMPIONS','TOP500']
+leagueranks=['IRON','BRONZE','SILVER','GOLD','PLATINUM','EMERALD','DIAMOND','MASTER','GRANDMASTER','CHALLENGER']
+valranks=['IRON','BRONZE','SILVER','GOLD','PLATINUM','DIAMOND','ASCENDANT','IMMORTAL','RADIANT']
 @app.route('/account/create')
 def create():
+    games={'CSGO':csranks,'Overwatch':owranks,'League':leagueranks,'Valorant':valranks}
     return render_template("requestpage.html")
