cf28d54a6d11e79c9c7e93541845d57161d45353
Mishibla <christopherjongue@gmail.com>
2024-05-04 16:47:58 +0800

    Added checkbox for exclusive skins and allowed users to upload max of 4 images

--- app/static/requestpage.js
+++ app/static/requestpage.js
@@ -1,39 +1,52 @@
-document.getElementById("submited").addEventListener("click", function() {
-    alert("Hello World!");
-  });
+$(document).ready(function() {
+  //linking the games dropdown with its corresponding rank
+  const firstDropdown = $('#games');
+  const secondDropdown = $('#ranks');
+  const csranks=['SILVER','GOLD NOVA','MASTER GUARDIAN','LEGENDARY']
+  const owranks=['BRONZE','SILVER','GOLD','PLATNIUM','DIAMOND','MASTER','GRANDMASTER','CHAMPIONS','TOP500']
+  const leagueranks=['IRON','BRONZE','SILVER','GOLD','PLATINUM','EMERALD','DIAMOND','MASTER','GRANDMASTER','CHALLENGER']
+  const valranks=['IRON','BRONZE','SILVER','GOLD','PLATINUM','DIAMOND','ASCENDANT','IMMORTAL','RADIANT']
+  const gamesapp={
+      'CSGO':csranks,'Overwatch':owranks,'League':leagueranks,'Valorant':valranks};
+  //linking skins with exclusive skins
+  const skin = $("#skins");
+  const exclusivity = $('#exclusive');
+  const label_exclu = $('#exclu')
+  //counting number of files
+
+  function updateSecondDropdown() {
+    const category = firstDropdown.val();
+    // Clear existing options
+    secondDropdown.html('<option value="">Select an option</option>');
+    // Add options based on the selected category
+    gamesapp[category].forEach(option => {
+      const optionElement = $('<option>').text(option).val(option);
+      secondDropdown.append(optionElement);
+    });
+    // Show the second dropdown
+    secondDropdown.css('display', 'inline');
+  }
+  //function to handle the exclusive checkbox
+  function exclusives() {
+    if (skin.val() === 'yes') {
+      label_exclu.show();
+      exclusivity.show();
+    } else {
+      exclusivity.hide();
+      label_exclu.hide();
+    }
+  }
 
 
-// Get references to the dropdowns
-const firstDropdown = document.getElementById('firstDropdown');
-const secondDropdown = document.getElementById('secondDropdown');
-
-const csranks=['SILVER','GOLD NOVA','MASTER GUARDIAN','LEGENDARY']
-const owranks=['BRONZE','SILVER','GOLD','PLATNIUM','DIAMOND','MASTER','GRANDMASTER','CHAMPIONS','TOP500']
-const leagueranks=['IRON','BRONZE','SILVER','GOLD','PLATINUM','EMERALD','DIAMOND','MASTER','GRANDMASTER','CHALLENGER']
-const valranks=['IRON','BRONZE','SILVER','GOLD','PLATINUM','DIAMOND','ASCENDANT','IMMORTAL','RADIANT']
-const gamesapp={
-    'CSGO':csranks,'Overwatch':owranks,'League':leagueranks,'Valorant':valranks};
-// Define options for the second dropdown based on the selection in the first dropdown
-const optionsByCategory = {
-  '1': ['Option 1.1', 'Option 1.2', 'Option 1.3'],
-  '2': ['Option 2.1', 'Option 2.2', 'Option 2.3']
-};
-
-// Function to update options of the second dropdown based on the selection in the first dropdown
-function updateSecondDropdown() {
-  const category = firstDropdown.value;
-  // Clear existing options
-  secondDropdown.innerHTML = '<option value="">Select an option</option>';
-  // Add options based on the selected category
-  gamesapp[category].forEach(option => {
-    const optionElement = document.createElement('option');
-    optionElement.textContent = option;
-    optionElement.value = option;
-    secondDropdown.appendChild(optionElement);
-  });
-  // Show the second dropdown
-  secondDropdown.style.display = 'inline';
-}
-
-// Event listener to trigger update of the second dropdown when the first dropdown selection changes
-firstDropdown.addEventListener('change', updateSecondDropdown);
+  // Event listener to trigger update of the second dropdown when the first dropdown selection changes
+  firstDropdown.change(updateSecondDropdown);
+  skin.change(exclusives);
+  //only allows max 4 images
+  $('#images').on('change', function(event) {
+    const selectedFilesCount = $(this)[0].files.length;
+    if (selectedFilesCount > 4) {
+      alert('You can only select up to 4 files.');
+      $(this).val(null); // Clear the file input
+    }
+  })
+});
