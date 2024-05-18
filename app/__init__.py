b88a72748a76439723651e54ff63e8de4c3dced8
Mishibla <christopherjongue@gmail.com>
2024-05-18 18:12:53 +0800

    Added blueprints

--- app/__init__.py
+++ app/__init__.py
@@ -4,10 +4,20 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager, current_user
 
-app = Flask(__name__)
-app.config.from_object(Config)
 db = SQLAlchemy(app)
-migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
+
+
+def create_app(config):
+    app = Flask(__name__)
+    app.config.from_object(config)
+
+    from app.blueprints import main
+    app.register_blueprint(main)
+    db.init_app(app)
+    login.init_app(app)
+
+    return app
+
 from app import routes,models
---
+++ app/tests/unit.py
@@ -0,0 +1,9 @@
+from unittest import TestCase
+
+class BasicUnitTests(TestCase):
+
+
+    def setUp(self):
+        pass
+    def tearDown(self):
+        pass
