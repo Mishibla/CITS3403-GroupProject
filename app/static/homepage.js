9fc1e08e27ac815cadbffea684a597bebcd75a97
afiqhassani <afiq.hassani@mac.com>
2024-05-19 16:41:22 +0800

    updated homepage.js

--- app/static/homepage.js
+++ app/static/homepage.js
@@ -17,6 +17,26 @@
             }
         }
     }
+    
+        document.addEventListener('DOMContentLoaded', function() {
+        const ads = {{ ads|tojson }};
+        const fileExtensions = ['jpg', 'jpeg', 'png'];
+        
+        ads.forEach((ad, index) => {
+            const adId = ad.ad_id;
+            const imageElement = document.querySelector(`div[data-image="image${index + 1}"]`);
+            if (imageElement) {
+                for (const extension of fileExtensions) {
+                    const imageUrl = `/static/uploads/${adId}/${adId}_1.${extension}`;
+                    fetch(imageUrl).then(response => {
+                        if (response.ok) {
+                            imageElement.style.backgroundImage = `url('${imageUrl}')`;
+                        }
+                    });
+                }
+            }
+        });
+    });
 
     // Toggle dropdown content
     document.getElementById('expandButton').addEventListener('click', function() {
@@ -27,3 +47,4 @@
             content.classList.add('show');
         }
     });
+    
